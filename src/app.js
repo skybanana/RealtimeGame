0a6bce64d27718a9bee90e9f589bb0a2dcb04b8f
skybanana <112135946+skybanana@users.noreply.github.com>
2024-10-07 12:31:58 +0900

    fix : app url

--- src/app.js
+++ src/app.js
@@ -11,7 +11,10 @@ const PORT = 3000;
 
 app.use(express.static('public'));
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: "http://54.180.122.123:3000",
+  credentials: true,
+}));
 app.use(express.urlencoded({ extended: false }));
 initSocket(server);
 
